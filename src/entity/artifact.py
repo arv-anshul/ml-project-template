8aa47651004e4c01071b3e6cedc2775df53f9320
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-08-11 01:18:28 +0530

    Refactor src.entity.artifact

--- src/entity/artifact.py
+++ src/entity/artifact.py
@@ -4,40 +4,40 @@ from dataclasses import dataclass
 from pathlib import Path
 
 
-@dataclass
+@dataclass(frozen=True, eq=False, slots=True)
 class DataIngestionArtifact:
-    feature_store_fp: Path
+    feature_store_path: Path
     train_path: Path
     test_path: Path
 
 
-@dataclass
+@dataclass(frozen=True, eq=False, slots=True)
 class DataValidationArtifact(DataIngestionArtifact):
-    report_fp: Path
+    report_path: Path
 
 
-@dataclass
+@dataclass(frozen=True, eq=False, slots=True)
 class DataTransformationArtifact:
     transformer_pkl: Path
-    target_enc_fp: Path
+    target_enc_path: Path
     train_npz_path: Path
     test_npz_path: Path
 
 
-@dataclass
+@dataclass(frozen=True, eq=False, slots=True)
 class ModelTrainerArtifact:
     model_path: Path
     train_score: float
     test_score: float
 
 
-@dataclass
+@dataclass(frozen=True, eq=False, slots=True)
 class ModelEvaluationArtifact:
     is_model_accepted: bool
     improved_accuracy: float
 
 
-@dataclass
+@dataclass(frozen=True, eq=False, slots=True)
 class ModelPusherArtifact:
     self_dir: Path
     stored_model_dir: Path
